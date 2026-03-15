c8db7d31a42dd565c64c4ea1dc88b53fd9bfa5f3
Gururaakesh <gururaakesh@gmail.com>
2026-03-15 16:16:12 +0530

    upated successfully

--- app.py
+++ app.py
@@ -85,3 +85,5 @@ def ai_question(id: int, question: str, db: Session = Depends(db_get)):
 
     return {"answer": answer}
 
+    # content=await file.read()
+    # return {"filename ":file.filename}
